0af5bbd81e422f859358ef82c6be9b9f04f6baac
merin86 <m_liljenroth@hotmail.com>
2023-12-19 14:59:05 +0000

    Add initial JavaScript code

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+const questions = [
+    {
+        question: "Which film did Stanley Kubrick not direct?",
+        answers: [
+            {text: "Lolita", correct: false},
+            {text: "Dr. Strangelove", correct: false},
+            {text: "12 Angry Men", correct: true},
+            {text: "Spartacus", correct: false},
+        ]
+    },
+    {
+        question: "Which film did Stanley Kubrick not direct?",
+        answers: [
+            {text: "Lolita", correct: false},
+            {text: "Dr. Strangelove", correct: false},
+            {text: "12 Angry Men", correct: true},
+            {text: "Spartacus", correct: false},
+        ]
+    }
+]
